e9ed0189ab5efd404bb77a20503bf1308a8c6c46
Kashish Jethwani <kashishjethwani7990@gmail.com>
2026-01-20 10:31:50 +0530

    Justify-between if app is not installed otherwise center the logo in navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,17 +2,23 @@ import React from "react";
 
 function Navbar({ deferredPrompt, onInstall }) {
   return (
-    <nav className="flex flex-col gap-4 sm:flex-row justify-between items-center px-10 py-6">
-      <h1 className="text-2xl font-bold tracking-wide">PWA Demo</h1>
+    <nav className="px-10 py-6">
+      <div
+        className={`flex items-center ${
+          deferredPrompt ? "justify-between" : "justify-center"
+        }`}
+      >
+        <h1 className="text-2xl font-bold">PWA Demo</h1>
 
-      {deferredPrompt && (
-        <button
-          onClick={onInstall}
-          className="px-5 py-2 rounded-lg bg-white text-black font-medium hover:bg-gray-200 transition"
-        >
-          Install App
-        </button>
-      )}
+        {deferredPrompt && (
+          <button
+            onClick={onInstall}
+            className="px-5 py-2 rounded-lg bg-white text-black font-medium hover:bg-gray-200 transition"
+          >
+            Install App
+          </button>
+        )}
+      </div>
     </nav>
   );
 }
